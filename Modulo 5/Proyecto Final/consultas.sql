3c18cfd9e9d8b7c2409a11f9e27a1508655c98bc
Felipe Urdanivia Martínez <156330257+FelipeUrdanivia@users.noreply.github.com>
2024-04-02 13:45:48 -0300

    Add files via upload

---
+++ Modulo 5/Proyecto Final/consultas.sql
@@ -0,0 +1,34 @@
+SELECT
+	COALESCE(s.nombre, pr.nombre) AS nombre,
+	s.sueldo AS sueldo_soltera,
+	pr.sueldo AS sueldo_papi,
+	s.sueldo + pr.sueldo AS sueldo_total
+FROM
+	reparto_soltera_otra_vez s
+INNER JOIN
+	reparto_papi_ricky pr ON s.nombre = pr.nombre
+ORDER BY
+	actor;
+
+#####################
+
+SELECT
+	rs.nombre AS actor_soltera,
+	rs.sueldo AS sueldo_soltera
+FROM
+	reparto_soltera_otra_vez rs
+LEFT JOIN
+	reparto_papi_ricky rp ON rs.nombre = rp.nombre
+WHERE
+	rp.nombre IS NULL AND rs.sueldo > 90;
+
+######################
+
+SELECT COALESCE(s.nombre, pr.nombre) AS nombre,
+       COALESCE(s.sueldo, pr.sueldo) AS sueldo
+FROM reparto_soltera_otra_vez s
+FULL OUTER JOIN reparto_papi_ricky pr
+ON s.nombre = pr.nombre
+WHERE (s.nombre IS NULL OR pr.nombre IS NULL)
+      AND (s.sueldo < 85 OR pr.sueldo < 85);
+
